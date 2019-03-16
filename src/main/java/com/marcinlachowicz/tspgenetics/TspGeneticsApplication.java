e556e79314ab3727b0b97fe124ef591b918030a8
Lachowicz, Marcin <lachowicz.marcin1@gmail.com>
2019-03-16 18:27:04 +0100

    Initial commit

---
+++ src/main/java/com/marcinlachowicz/tspgenetics/TspGeneticsApplication.java
@@ -0,0 +1,13 @@
+package com.marcinlachowicz.tspgenetics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TspGeneticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TspGeneticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcinlachowicz/tspgenetics/TspGeneticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcinlachowicz.tspgenetics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TspGeneticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
